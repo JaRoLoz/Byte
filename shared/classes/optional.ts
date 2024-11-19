f5914028ba178ce2605aa6837551a0454a86e6b7
Jaime Rodrigo <40668121+JaRoLoz@users.noreply.github.com>
2024-11-19 23:57:50 +0100

    feat: Refactor Optional class to include isSome flag for improved value checking

--- shared/classes/optional.ts
+++ shared/classes/optional.ts
@@ -5,9 +5,11 @@
  */
 export class Optional<T = any | undefined> {
     private value: T;
+    private _isSome: boolean;
 
-    private constructor(value: T) {
+    private constructor(value: T, isSome: boolean) {
         this.value = value;
+        this._isSome = isSome;
     }
 
     /**
@@ -18,11 +20,11 @@ export class Optional<T = any | undefined> {
     /**
      * @returns Where the wrapped value is null
      */
-    public isSome = (): boolean => this.value !== undefined;
+    public isSome = (): boolean => this._isSome;
 
     /** @noSelf **/
-    public static Some = <T>(value: T): Optional<T> => new Optional<T>(value);
+    public static Some = <T>(value: T): Optional<T> => new Optional<T>(value, true);
     /** @noSelf **/
-    public static None = <T>(): Optional<T> => new Optional<T>(undefined as T);
+    public static None = <T>(): Optional<T> => new Optional<T>(undefined as T, false);
 }
 
