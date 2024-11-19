d45bc783f138d1c2aa627b9c5f473859d9bf008a
Jaime Rodrigo <40668121+JaRoLoz@users.noreply.github.com>
2024-11-19 19:07:24 +0100

    refactor: Update EnvManager methods to include return types for better type safety

--- server/utils/env.ts
+++ server/utils/env.ts
@@ -1,11 +1,11 @@
 /** @noSelf **/
 export class EnvManager {
     private constructor() {}
-    public static getMaxClient = () => GetConvarInt("sv_maxclients", 48);
-    public static getGameBuild = () => GetConvarInt("sv_enforceGameBuild", 1604);
-    public static getTags = () => GetConvar("tags", "");
-    public static getSteamWebApiKey = () => GetConvar("steam_webApiKey", "none");
-    public static getServerClosed = () => GetConvarInt("serverClosed", 1) == 1;
-    public static getDebug = () => GetConvarInt("debug", 0) == 1;
-    public static getProduction = () => GetConvarInt("production", 0) == 1;
+    public static getMaxClient = (): number | 48 => GetConvarInt("sv_maxclients", 48);
+    public static getGameBuild = (): number | 1604 => GetConvarInt("sv_enforceGameBuild", 1604);
+    public static getTags = (): string | "" => GetConvar("tags", "");
+    public static getSteamWebApiKey = (): string | "none" => GetConvar("steam_webApiKey", "none");
+    public static getServerClosed = (): boolean | true => GetConvarInt("serverClosed", 1) == 1;
+    public static getDebug = (): boolean | false => GetConvarInt("debug", 0) == 1;
+    public static getProduction = (): boolean | false => GetConvarInt("production", 0) == 1;
 }
