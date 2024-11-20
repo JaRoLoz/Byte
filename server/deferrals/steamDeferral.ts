629e5c2a72cb409b9649f7a44c8e51f9882ae28a
Jaime Rodrigo <40668121+JaRoLoz@users.noreply.github.com>
2024-11-20 11:15:10 +0100

    docs: Document the steamDeferral for more context

--- server/deferrals/steamDeferral.ts
+++ server/deferrals/steamDeferral.ts
@@ -5,6 +5,10 @@ import { Deferral } from "./deferralManager";
 
 const translator = getTranslator();
 
+/**
+ * This deferral checks if the connecting player does not have steam running.
+ * This is on pourpose to prevent some cheaters from joining as some cheats such as eulen hook the steam overlay to draw the cheat interface.
+ */
 const steamDeferral: Deferral = (src, playerName, setKickReason, deferrals) => {
     if (EnvManager.getDebug() || EnvManager.getSteamWebApiKey() === "none") return Ok(null);
     const user = new User(src);
