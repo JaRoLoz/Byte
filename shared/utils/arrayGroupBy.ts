70e35414399569971fe91d6bf5ae4ff0801394cd
Jaime Rodrigo <40668121+JaRoLoz@users.noreply.github.com>
2024-12-12 21:12:32 +0100

    feat: Add arrayGroupBy function for convenience

---
+++ shared/utils/arrayGroupBy.ts
@@ -0,0 +1,7 @@
+
+// https://stackoverflow.com/questions/42136098/array-groupby-in-typescript
+export const arrayGroupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
+    arr.reduce((groups, item) => {
+        (groups[key(item)] ||= []).push(item);
+        return groups;
+    }, {} as Record<K, T[]>);
--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./uuid";
 export * from "./parseCoords";
+export * from "./arrayGroupBy";
