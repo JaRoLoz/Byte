238187ebb39be838bde0f414c442b973efd7ded4
Jaime Rodrigo <40668121+JaRoLoz@users.noreply.github.com>
2024-12-18 15:21:56 +0100

    feat: Update Result<T, E> to use LuaMultiReturn

--- shared/classes/result.ts
+++ shared/classes/result.ts
@@ -6,23 +6,23 @@ type Null = null | undefined;
  * Tries to take the best of Go's and Rust's error handling.
  */
 export type Result<T = Null, E = Null> =
-    [error: true, value: E]
-    | [error: false, value: T];
+    LuaMultiReturn<[error: true, value: E]>
+    | LuaMultiReturn<[error: false, value: T]>;
 
 /**
  * Helper function to create an successful result
  * @param value The value to wrap
  * @returns A successful result
  */
-export const Ok = <T, E>(value: T): Result<T, E> => [false, value];
+export const Ok = <T, E>(value: T): Result<T, E> => $multi(false, value) as LuaMultiReturn<[error: false, value: T]>;
 /**
  * Helper function to create an empty successful result
  * @returns An empty successful result
  */
-export const EmptyOk = <E>(): Result<Null, E> => [false, undefined];
+export const EmptyOk = <E>(): Result<Null, E> => $multi(false, undefined) as LuaMultiReturn<[error: false, value: Null]>;
 /**
  * Helper function to create an error result
  * @param error The error to wrap
  * @returns An error result
  */
-export const Err = <E>(error?: E): Result<never, E> => [true, error as E];
+export const Err = <E>(error?: E): Result<never, E> => $multi(true, error as E) as LuaMultiReturn<[error: true, value: E]>;
