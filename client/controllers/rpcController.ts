37261db5d9849704955e605fdd3ba8e5fe7be7f8
Jaime Rodrigo <40668121+JaRoLoz@users.noreply.github.com>
2024-12-18 22:12:47 +0100

    feat: Add client-side rpc & improved security

--- client/controllers/rpcController.ts
+++ client/controllers/rpcController.ts
@@ -1,19 +1,34 @@
 import { getEventNames } from "../shared/classes/eventNameController";
 import { CfxPromise } from "../shared/native_defs";
+import { uuid, UUID } from "../shared/utils/uuid";
 
 const eventNames = getEventNames();
+export type RPCCallback<T extends any[] = any> = (this: void, cb: (this: void, ...args: T) => void, ...args: any[]) => any;
 
 export class RPCController {
     /** @noSelf **/
     private static instance: RPCController;
-    private promises: Record<string, CfxPromise<unknown[]>> = {};
+    private promises: Record<UUID, CfxPromise<unknown[]>> = {};
+    private procedures: Record<string, RPCCallback> = {};
 
     private constructor() {
-        RegisterNetEvent(eventNames.get("Client.RPC.Callback"), (procuedure: string, args: Array<any>) => {
-            if (!this.promises[procuedure]) return;
+        RegisterNetEvent(eventNames.get("Client.RPC.Callback"), (procuedureId: UUID, args: Array<any>) => {
+            const procedurePromise = this.promises[procuedureId] as CfxPromise<any> | undefined;
+            if (!procedurePromise) return;
 
-            const procedurePromise = this.promises[procuedure] as CfxPromise<any>;
             procedurePromise.resolve(procedurePromise, args);
+        });
+
+        RegisterNetEvent(eventNames.get("Client.RPC.Call"), (procedure: string, procedureId: UUID, args: Array<any>) => {
+            const procedureCallback = this.procedures[procedure];
+            if (!procedureCallback) return;
+
+            procedureCallback(
+                (...retVals: any[]) => {
+                    TriggerServerEvent(eventNames.get("Server.RPC.Callback"), procedureId, retVals);
+                },
+                ...args
+            );
         });
     }
 
@@ -26,11 +41,33 @@ export class RPCController {
      */
     public call = <T extends any[] = any>(procedure: string, ...args: any[]): LuaMultiReturn<T> => {
         const procedurePromise = promise.new();
-        this.promises[procedure] = procedurePromise;
+        const procedureId = uuid();
+        this.promises[procedureId] = procedurePromise;
 
-        TriggerServerEvent(eventNames.get("Server.RPC.Call"), procedure, args);
+        TriggerServerEvent(eventNames.get("Server.RPC.Call"), procedure, procedureId, args);
 
-        return $multi(...Citizen.Await<T>(procedurePromise));
+        const result = Citizen.Await<T>(procedurePromise);
+        delete this.promises[procedureId];
+
+        return $multi(...result);
+    };
+
+    /**
+     * Registers a procedure that can be called from the server.
+     * @template Args An array containing the types of the data returned by the procedure to the server.
+     * @param procedure The name of the procedure to register.
+     * @param callback The callback function to call when the procedure is called.
+     */
+    public registerProcedure = <T extends any[] = any>(procedure: string, callback: RPCCallback<T>) => {
+        this.procedures[procedure] = callback;
+    };
+
+    /**
+     * Deletes a procedure handler from the client.
+     * @param procedure The name of the procedure to delete.
+     */
+    public deleteProcedure = (procedure: string) => {
+        delete this.procedures[procedure];
     };
 
     /** @noSelf **/
--- server/controllers/rpcController.ts
+++ server/controllers/rpcController.ts
@@ -1,4 +1,6 @@
 import { getEventNames } from "../shared/classes/eventNameController";
+import { CfxPromise } from "../shared/native_defs";
+import { uuid, UUID } from "../shared/utils/uuid";
 
 const eventNames = getEventNames();
 
@@ -12,30 +14,73 @@ export type RPCCallback<T extends any[] = any> = (this: void, src: number, cb: (
 export class RPCController {
     /** @noSelf **/
     private static instance: RPCController;
-    private callbacks: Record<string, RPCCallback> = {};
+    private promises: Record<UUID, { source: number, promise: CfxPromise<unknown[]> }> = {};
+    private procedures: Record<string, RPCCallback> = {};
 
     private constructor() {
-        RegisterNetEvent(eventNames.get("Server.RPC.Call"), (procedure: string, args: Array<any>) => {
+        RegisterNetEvent(eventNames.get("Server.RPC.Call"), (procedure: string, procedureId: UUID, args: Array<any>) => {
             const src = source;
-            if (!this.callbacks[procedure]) return;
+            const procedureCallback = this.procedures[procedure];
+            if (!procedureCallback) return;
 
-            const procedureCallback = this.callbacks[procedure];
             procedureCallback(
                 src,
                 (...retVals: any[]) => {
-                    TriggerClientEvent(eventNames.get("Client.RPC.Callback"), src, procedure, retVals);
+                    TriggerClientEvent(eventNames.get("Client.RPC.Callback"), src, procedureId, retVals);
                 },
                 ...args
             );
         });
+
+        RegisterNetEvent(eventNames.get("Server.RPC.Callback"), (procuedureId: UUID, args: Array<any>) => {
+            const src = source;
+            const procedurePromise = this.promises[procuedureId];
+            if (!procedurePromise) return;
+            if (procedurePromise.source !== src) return;
+
+            procedurePromise.promise.resolve(procedurePromise.promise, args);
+        });
     }
 
+    /**
+     * Registers a procedure that can be called from the client.
+     * @template Args An array containing the types of the data returned by the procedure to the client.
+     * @param procedure The name of the procedure to register.
+     * @param callback The callback function to call when the procedure is called.
+     */
     public registerProcedure = <T extends any[] = any>(procedure: string, callback: RPCCallback<T>) => {
-        this.callbacks[procedure] = callback;
+        this.procedures[procedure] = callback;
     };
 
+    /**
+     * Deletes a procedure handler from the server.
+     * @param procedure The name of the procedure to delete.
+     */
     public deleteProcedure = (procedure: string) => {
-        delete this.callbacks[procedure];
+        delete this.procedures[procedure];
+    };
+
+    /**
+     * Calls a procedure on the client.
+     * @param src The player source to call the procedure on.
+     * @param procedure The name of the procedure to call.
+     * @param args The arguments to pass to the client.
+     * @returns A promise that resolves when the client responds.
+     */
+    public call = <T extends any[] = any>(src: number, procedure: string, ...args: any[]): LuaMultiReturn<T> => {
+        const procedurePromise = promise.new();
+        const procedureId = uuid();
+        this.promises[procedureId] = {
+            source: src,
+            promise: procedurePromise
+        };
+
+        TriggerClientEvent(eventNames.get("Client.RPC.Call"), src, procedure, procedureId, args);
+
+        const result = Citizen.Await<T>(procedurePromise);
+        delete this.promises[procedureId];
+
+        return $multi(...result);
     };
 
     /** @noSelf **/
